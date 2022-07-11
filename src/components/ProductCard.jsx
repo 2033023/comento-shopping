94095799096cb2c9adeabc911062b254eb7f2aa8
2033023 <yunji6144@gmail.com>
2022-07-11 22:59:12 +0900

    테마 버튼 클릭 시 상품 리스트가 바뀌는 로직 구현

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,31 +1,42 @@
 import styled from "styled-components";
 
-const ProductCard = ({ name, description, thumbnail }) => {
+const ProductCard = ({ name, description, thumbnail, onClick }) => {
   return (
-    <div>
-      <img height="204" width = "341" top ="3" src={thumbnail} alt={name} />
+    <ProductCardStyled onClick={onClick}>
+       <ProductThumbnail style={{ backgroundImage: `url(${thumbnail})` }} />
       <ProductName>{name}</ProductName>
       <ProductDescription>{description}</ProductDescription>
-    </div>
+      </ProductCardStyled>
   );
 };
 
-const ProductName = styled.div`
-height: 26px;
-width: 341px;
-font-weight: 700;
-font-size: 20px;
-line-height: 26px;
-letter-spacing: -0.01em;
+
+const ProductCardStyled = styled.div`
+  padding-bottom: 40px;
 `;
+
+
+const ProductThumbnail = styled.div`
+  width: 341px;
+  height: 204px;
+  background-size: cover;
+  background-position: center;
+`;
+
+const ProductName = styled.div`
+  font-weight: 700;
+  font-size: 20px;
+  line-height: 26px;
+  letter-spacing: -0.01em;
+  padding-bottom: 12px;
+`;
+
 const ProductDescription = styled.div`
-height: 42px;
-width: 323px;
-font-weight: 400;
-font-size: 16px;
-line-height: 21px;
-text-align: left;
-letter-spacing: -0.01em;
+ font-weight: 400;
+  font-size: 16px;
+  line-height: 21px;
+  letter-spacing: -0.01em;
+  color: #000000;
 `;
 
 export default ProductCard;
--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,5 +1,45 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
+import ProductCard from "../components/ProductCard";
+import Navigation from "../components/Navigation";
+import { getProductDetail } from "../data/mockData";
+
 const ProductDetail = () => {
-  return <div>상품상세 페이지</div>;
+  // URL에서 paramter 변수(productId) 받아오는 로직
+  let { productId } = useParams();
+  const [product, setProduct] = useState();
+
+  useEffect(() => {
+    const result = getProductDetail(productId);
+    setProduct(result);
+  }, []);
+
+
+
+  return (
+    <ProductDetailStyled>
+      <TitleSection>
+        <Navigation NavigationName={"코멘토 쇼핑"}/>
+      </TitleSection>
+
+      <ProductCard
+              key={product.id}
+              thumbnail={product.thumbnail}
+            />
+    </ProductDetailStyled>
+  );
 };
+
+
+
+const ProductDetailStyled = styled.div``;
+
+const TitleSection = styled.div`
+width: 390px;
+height: 66px;
+`;
+
+
 
 export default ProductDetail;
