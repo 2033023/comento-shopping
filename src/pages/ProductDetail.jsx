e48436f5932ce983d23c86761245e74392827cb1
2033023 <yunji6144@gmail.com>
2022-07-11 23:34:23 +0900

    테마 버튼 클릭 시 상품 리스트가 바뀌는 로직 구현

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -23,10 +23,6 @@ const ProductDetail = () => {
         <Navigation NavigationName={"코멘토 쇼핑"}/>
       </TitleSection>
 
-      <ProductCard
-              key={product.id}
-              thumbnail={product.thumbnail}
-            />
     </ProductDetailStyled>
   );
 };
