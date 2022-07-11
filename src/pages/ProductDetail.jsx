eb69ba3828fd20169c73ee1ba7347f3a2797d59e
2033023 <yunji6144@gmail.com>
2022-07-11 23:43:16 +0900

    테마 버튼 클릭 시 상품 리스트가 바뀌는 로직 구현

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,19 +1,18 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
-import ProductCard from "../components/ProductCard";
 import Navigation from "../components/Navigation";
 import { getProductDetail } from "../data/mockData";
 
 const ProductDetail = () => {
   // URL에서 paramter 변수(productId) 받아오는 로직
   let { productId } = useParams();
-  const [product, setProduct] = useState();
+  const [product, setProduct] = useState(); // eslint-disable-line no-unused-vars
 
   useEffect(() => {
     const result = getProductDetail(productId);
     setProduct(result);
-  }, []);
+  }, [setProduct]);
 
 
 
